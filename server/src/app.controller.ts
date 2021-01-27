055ba53909d7d2a4f55e3b4aa3e839f3605f584c
burach0k <burak_andrej@mail.ru>
2021-01-27 20:08:53 +0300

    test config

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,9 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Client } from 'pg'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    });
+
+    client.connect();
+
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+      if (err) throw err;
+      console.log('jepa');
+      client.end();
+    });
+
+
+  }
 
   // @Get()
   // getHello(): string {
